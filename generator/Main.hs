bc7a1e61ebb924c44284de1768518eff18ea3c1f
Reuben D'Netto <rdnetto@gmail.com>
2018-12-20 18:10:46 +1100

    Replaced JSON parsing with YAML

--- generator/Main.hs
+++ generator/Main.hs
@@ -1,17 +1,13 @@
 module Main where
 
 import BasicPrelude
-import Data.Aeson (eitherDecodeFileStrict)
 import Data.Swagger (Swagger)
+import Data.Yaml (decodeFileThrow)
 
 
 main :: IO ()
 main = do
   swagger :: Swagger
-          <- throwLeft <$> eitherDecodeFileStrict "swagger.json"
+          <- decodeFileThrow "openapi-directory/APIs/trello.com/1.0/swagger.yaml"
   print swagger
-
-throwLeft :: Either String a -> a
-throwLeft (Left msg) = error msg
-throwLeft (Right x)  = x
 
