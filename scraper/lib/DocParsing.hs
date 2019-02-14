46463a54d5c2cd6b24f166fd909a80e526c94d25
Reuben D'Netto <rdnetto@gmail.com>
2019-02-14 19:05:08 +1100

    WIP DocParsing code, now has unit tests.
    Refactored project structure to enable testing

---
+++ scraper/lib/DocParsing.hs
@@ -0,0 +1,131 @@
+module DocParsing {- (extractExampleResponses, inferSchema) -} where
+
+import BasicPrelude hiding (encodeUtf8)
+import Data.Aeson (FromJSON, Value(..), encode, eitherDecode)
+import qualified Data.ByteString.Lazy as BSL
+import qualified Data.HashMap.Strict as HMS
+import qualified Data.Text as T
+import Data.Text.Encoding (encodeUtf8)
+import qualified Data.Vector as V
+import Lens.Micro ((^..), (&))
+import Lens.Micro.Aeson (_String, key, values)
+import Safe (fromJustNote)
+import Text.Parsec (ParseError, many, parse, eof, try, manyTill)
+import Text.Parsec.Char (string, anyChar)
+
+import Debug.Trace
+import Util
+
+
+-- Given a documentation blob, extracts a map of operation ID to sample response
+extractExampleResponses :: Value -> HashMap Text Value
+extractExampleResponses
+  = HMS.fromList
+  . catMaybes
+  . map getResponse
+  . filter (
+    (==) "endpoint"
+    . fromJustNote "Expected a single `type`"
+    . getStringKey "type"
+  )
+  . getAllDocs
+
+
+-- Traverse structure recursively to get a complete list of documents
+-- TODO: make this recursive
+getAllDocs :: Value -> [Value]
+getAllDocs (Array v) = V.toList v
+getAllDocs x = error $ "Invalid document root: " ++ show x
+
+
+-- Converts a given document to a (operationId, exampleResponse)
+getResponse :: Value -> Maybe (Text, Value)
+getResponse obj = do
+  operationId <- getStringKey "slug" obj
+  body <- getStringKey "body" obj
+  guard $ body /= ""
+
+  codeBlockContents :: [Text]
+    <- (case extractCodeBlockContents (T.unpack . decodeUtf8 . BSL.toStrict $ encode obj) body of
+            Right x -> Just x
+            Left e -> traceShow e $ Nothing
+       )
+
+  let decoder = decodeJson $ "Failed to decode code blobs in " ++ show obj
+      responses :: [Value]
+        = codeBlockContents
+        & map decoder
+        & map (\blob -> blob ^.. key "codes" . values)
+        & join
+        & filter isJsonCodeBlob
+        & map (getStringKey "code")
+        & map (fromJustNote "Could not find `code` attribute")
+        & map decoder
+
+  let response = case responses of
+                      [x] -> x
+                      _   -> error $ "Could not find unique response for " ++ show obj
+  return (operationId, response)
+
+{- Body has the following structure:
+   Optional header explaining stuff.
+   [block:code]
+   {
+     "codes": [{
+       "code": "$JSON",
+       "language": "json"
+     }]
+   }
+   [/block]
+
+   Note that there may be multiple code blocks in some cases, though not all of them may be JSON.
+   e.g. 'http' is used for examples of query params
+-}
+-- TODO: see how many blocks don't parse
+extractCodeBlockContents :: String -> Text -> Either ParseError [Text]
+extractCodeBlockContents name raw = parse (many codeBlockParser <* eof) name raw where
+  codeBlockParser = do
+    void $ manyTill anyChar (try $ string "[block:code]")
+    txt <- manyTill anyChar (try $ string "[/block]")
+    return $ T.pack txt
+
+-- Determines if a given code block contains JSON
+isJsonCodeBlob :: Value -> Bool
+isJsonCodeBlob
+  = (==) "json"
+  . fromJustNote "Only one `language` expected"
+  . getStringKey "language"
+
+
+-- TODO: given a JSON object, infer a schema for it
+inferSchema :: Value -> Value
+inferSchema = undefined
+
+
+-- Generic helper for extracting a string from a JSOn object
+getStringKey :: Text -> Value -> Maybe Text
+getStringKey k obj
+  =   zeroOrOne
+  $   obj
+  ^.. key k
+  .   _String
+
+-- Helper function for coercing the result of a traversal to a single value
+singletonNote :: Show a => String -> [a] -> a
+singletonNote _ [x] = x
+singletonNote msg xs = error $ msg ++ ": " ++ show xs
+
+zeroOrOne :: Show a => [a] -> Maybe a
+zeroOrOne [x] = Just x
+zeroOrOne []  = Nothing
+zeroOrOne xs = error $ "Expected <2 results: " ++ show xs
+
+-- Deserializes a JSON snippet
+decodeJson :: FromJSON a => String -> Text -> a
+decodeJson err txt
+  = fromRightNote (err ++ ": " ++ T.unpack txt)
+  . eitherDecode
+  . BSL.fromStrict
+  . encodeUtf8
+  $ txt
+
---
+++ scraper/test/Spec.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module Main (main) where
+
+import BasicPrelude
+import Data.Either (isRight)
+import Test.Tasty (TestTree, defaultMain, testGroup)
+import Test.Tasty.HUnit (testCase, assertBool)
+import Text.RawString.QQ (r)
+
+import DocParsing
+
+
+main :: IO ()
+main
+  = defaultMain $ testGroup "Tests" [
+      docParseTests
+    ]
+
+docParseTests :: TestTree
+docParseTests
+  = testGroup "extractCodeBlockContents" (map test cases)
+  where
+    test x
+      = testCase "Code block test"
+      . assertBool (show res)
+      . isRight
+      $ res
+      where
+        res = extractCodeBlockContents "" x
+
+    cases = [
+        [r|
+          Here's some data
+          [block:parameters]
+          {
+            "data": {
+              "0-0": "**id**\nstring",
+              "0-1": "The ID of the action",
+              "1-0": "**data**\nobject",
+              "2-0": "**date**\ndate",
+              "2-1": "When the action occurred",
+              "3-0": "**idMemberCreator**\nstring",
+              "3-1": "The ID of the member who caused the action",
+              "4-0": "**type**\nstring",
+              "4-1": "The type of the action. See list of [Action Types](ref:action-types) for options.",
+              "1-1": "Relevant information regarding the action"
+            },
+            "cols": 2,
+            "rows": 5
+          }
+          [/block]
+          Here's some more
+          [block:code]
+          {
+            "codes": [
+              {
+                "code": "https://api.trello.com/1/actions/592f11060f95a3d3d46a987a",
+                "language": "curl"
+              }
+            ]
+          }
+          [/block]
+          And finally the JSON
+          [block:code]
+          {
+            "codes": [
+              {
+                "code": "{\n  \"id\": \"592f11060f95a3d3d46a987a\",\n  \"idMemberCreator\": \"5191197f9433cf5507006338\",\n  \"data\": {\n    \"list\": {\n      \"name\": \"Professional\",\n      \"id\": \"54a17e9db559f0356ce022e4\"\n    },\n    \"board\": {\n      \"shortLink\": \"BdarzfKF\",\n      \"name\": \"Life Goals\",\n      \"id\": \"54a17d76d4a5072e3931736b\"\n    },\n    \"card\": {\n      \"shortLink\": \"gplJv6dx\",\n      \"idShort\": 2,\n      \"name\": \"Increase revenue by 10%\",\n      \"id\": \"54a1844d304d9736e54d2546\",\n      \"due\": \"2017-12-12T17:00:00.000Z\"\n    },\n    \"old\": {\n      \"due\": \"2017-05-01T16:00:00.000Z\"\n    }\n  },\n  \"type\": \"updateCard\",\n  \"date\": \"2017-05-31T18:52:54.933Z\",\n  \"memberCreator\": {\n    \"id\": \"5191197f9433cf5507006338\",\n    \"avatarHash\": \"ae0fde383cc2a195c053f1ad42c02022\",\n    \"fullName\": \"Brian Cervino\",\n    \"initials\": \"BC\",\n    \"username\": \"brian\"\n  },\n  \"display\": {\n    \"translationKey\": \"action_changed_a_due_date\",\n    \"entities\": {\n      \"card\": {\n        \"type\": \"card\",\n        \"due\": \"2017-12-12T17:00:00.000Z\",\n        \"id\": \"54a1844d304d9736e54d2546\",\n        \"shortLink\": \"gplJv6dx\",\n        \"text\": \"Increase revenue by 10%\"\n      },\n      \"date\": {\n        \"type\": \"date\",\n        \"date\": \"2017-12-12T17:00:00.000Z\"\n      },\n      \"memberCreator\": {\n        \"type\": \"member\",\n        \"id\": \"5191197f9433cf5507006338\",\n        \"username\": \"brian\",\n        \"text\": \"Brian Cervino\"\n      }\n    }\n  }\n}",
+                "language": "json"
+              }
+            ]
+          }
+          [/block]
+        |]
+      ]
+
