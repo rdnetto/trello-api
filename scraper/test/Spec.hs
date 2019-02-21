004263f2281d8579cda08eda66cefa843cbe6d14
Reuben D'Netto <rdnetto@gmail.com>
2019-02-22 10:11:52 +1100

    Refactored tests to use external files for large input strings

--- scraper/test/Spec.hs
+++ scraper/test/Spec.hs
@@ -5,6 +5,7 @@ module Main (main) where
 import BasicPrelude
 import Data.Either (isRight)
 import Data.Text (stripEnd)
+import Data.Yaml (decodeFileThrow)
 import Test.Tasty (TestTree, defaultMain, testGroup)
 import Test.Tasty.HUnit (testCase, assertBool)
 import Text.RawString.QQ (r)
@@ -15,23 +16,27 @@ import DocParsing
 main :: IO ()
 main
   = defaultMain $ testGroup "Tests" [
-      docParseTests
+      docParseTests,
+      getResponseTests
     ]
+
+testCasePath :: FilePath
+testCasePath = "scraper/test/testcases"
 
 docParseTests :: TestTree
 docParseTests
   = testGroup "extractCodeBlockContents" (map test cases)
   where
-    test (name, input)
-      = testCase name
-      . assertBool (show res)
-      . isRight
-      $ res
-      where
-        res = extractCodeBlockContents "" (stripEnd input)
+    test (name, inputM) = testCase name $ do
+      input <- inputM
+      let res = extractCodeBlockContents "" (stripEnd input)
+
+      assertBool (show res)
+        . isRight
+        $ res
 
     cases = [
-        ("Simple case", [r|
+        ("Simple case", pure [r|
           [block:code]
           {
             "codes": [
@@ -44,52 +49,10 @@ docParseTests
           [/block]
         |]),
 
-        ("Multiple blocks", [r|
-          Here's some data
-          [block:parameters]
-          {
-            "data": {
-              "0-0": "**id**\nstring",
-              "0-1": "The ID of the action",
-              "1-0": "**data**\nobject",
-              "2-0": "**date**\ndate",
-              "2-1": "When the action occurred",
-              "3-0": "**idMemberCreator**\nstring",
-              "3-1": "The ID of the member who caused the action",
-              "4-0": "**type**\nstring",
-              "4-1": "The type of the action. See list of [Action Types](ref:action-types) for options.",
-              "1-1": "Relevant information regarding the action"
-            },
-            "cols": 2,
-            "rows": 5
-          }
-          [/block]
-          Here's some more
-          [block:code]
-          {
-            "codes": [
-              {
-                "code": "https://api.trello.com/1/actions/592f11060f95a3d3d46a987a",
-                "language": "curl"
-              }
-            ]
-          }
-          [/block]
-          And finally the JSON
-          [block:code]
-          {
-            "codes": [
-              {
-                "code": "{\n  \"id\": \"592f11060f95a3d3d46a987a\",\n  \"idMemberCreator\": \"5191197f9433cf5507006338\",\n  \"data\": {\n    \"list\": {\n      \"name\": \"Professional\",\n      \"id\": \"54a17e9db559f0356ce022e4\"\n    },\n    \"board\": {\n      \"shortLink\": \"BdarzfKF\",\n      \"name\": \"Life Goals\",\n      \"id\": \"54a17d76d4a5072e3931736b\"\n    },\n    \"card\": {\n      \"shortLink\": \"gplJv6dx\",\n      \"idShort\": 2,\n      \"name\": \"Increase revenue by 10%\",\n      \"id\": \"54a1844d304d9736e54d2546\",\n      \"due\": \"2017-12-12T17:00:00.000Z\"\n    },\n    \"old\": {\n      \"due\": \"2017-05-01T16:00:00.000Z\"\n    }\n  },\n  \"type\": \"updateCard\",\n  \"date\": \"2017-05-31T18:52:54.933Z\",\n  \"memberCreator\": {\n    \"id\": \"5191197f9433cf5507006338\",\n    \"avatarHash\": \"ae0fde383cc2a195c053f1ad42c02022\",\n    \"fullName\": \"Brian Cervino\",\n    \"initials\": \"BC\",\n    \"username\": \"brian\"\n  },\n  \"display\": {\n    \"translationKey\": \"action_changed_a_due_date\",\n    \"entities\": {\n      \"card\": {\n        \"type\": \"card\",\n        \"due\": \"2017-12-12T17:00:00.000Z\",\n        \"id\": \"54a1844d304d9736e54d2546\",\n        \"shortLink\": \"gplJv6dx\",\n        \"text\": \"Increase revenue by 10%\"\n      },\n      \"date\": {\n        \"type\": \"date\",\n        \"date\": \"2017-12-12T17:00:00.000Z\"\n      },\n      \"memberCreator\": {\n        \"type\": \"member\",\n        \"id\": \"5191197f9433cf5507006338\",\n        \"username\": \"brian\",\n        \"text\": \"Brian Cervino\"\n      }\n    }\n  }\n}",
-                "language": "json"
-              }
-            ]
-          }
-          [/block]
-        |]),
+        ("Multiple blocks", readFile $ testCasePath </> "docParseMultiBlock.txt"),
 
         -- This isn't a code block, so we expect no parse
-        ("Empty case", [r|
+        ("Empty case", pure [r|
           [block:callout]
           {
             "type": "danger",
@@ -99,4 +62,15 @@ docParseTests
           [/block]
       |])
       ]
+
+getResponseTests :: TestTree
+getResponseTests
+  = testGroup "getResponseTests" [
+      testResponse "getResponseFromApi",
+      testResponse "getResponseFromBody"
+    ]
+  where
+    testResponse name = testCase name $ do
+      actual <- getResponse <$> decodeFileThrow (testCasePath </> name ++ ".yaml")
+      assertBool (show actual) $ isJust actual
 
