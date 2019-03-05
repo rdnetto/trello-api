c03c023b7c17e51d5fcdc18448b0c00b66455e64
Reuben D'Netto <rdnetto@gmail.com>
2019-03-06 10:33:12 +1100

    Added sanity check to swagger patching, fixed typo

--- scraper/lib/Patches.hs
+++ scraper/lib/Patches.hs
@@ -3,12 +3,12 @@
 -- This module contains logic for patching the input files
 -- Expressing the changes in terms of structure rather than text makes them more robust against unrelated changes,
 -- but means that we need to implement our own logic for recognizing when the surrounding document has changed.
-module Patches where -- (patchDocs, patchSwagger) where
+module Patches (patchDocs, patchSwagger) where
 
 import BasicPrelude
 import Data.Aeson (Value(..))
 import qualified Data.Vector as V
-import Lens.Micro((.~), (%~), Traversal', filtered, toListOf, at)
+import Lens.Micro((.~), (%~), (&), Traversal', filtered, toListOf, at, has, each)
 import Lens.Micro.Aeson (_Object, _Array, key, values)
 import Lens.Micro.Platform ()
 
@@ -20,84 +20,93 @@ import Util
 patchSwagger :: Value -> Value
 patchSwagger = key "paths" %~ pathModifier where
   pathModifier
-    =
-    ( key "/boards/{id}/memberships/{idMembership}"
-      .  key "put"
-      .  key "parameters"
-      .  valuesWith "name" "idMembership"
-      .  keyAt "in"
-      .~ Just (String "path")
-    ) . (
-      key "/boards/{boardId}/boardStars"
-      .  key "get"
-      .  keyAt "summary"
-      .~ Just (String "/boards/{boardId}/boardStars")
-    ) . (
-      key "/boards/{boardId}/boardStars"
-      .  key "get"
-      .  key "parameters"
-      .  valuesWith "name" "filter"
-      .  keyAt "in"
-      .~ Just (String "query")
-    ) . (
-      key "/enterprises/{id}/members/{idMember}"
-      .  key "get"
-      .  key "parameters"
-      .  valuesWith "name" "idMember"
-      .  keyAt "in"
-      .~ Just (String "path")
-    ) . (
-      key "/members/{id}/savedSearches/{idSearch}"
-      .  key "get"
-      .  key "parameters"
-      .  _Array
-      %~ V.cons (mkPathParam "idSearch" "The ID of the search")
-    ) . (
-      key "/members/{id}/savedSearches/{idSearch}"
-      .  key "put"
-      .  key "parameters"
-      .  _Array
-      %~ V.cons (mkPathParam "idSearch" "The ID of the search")
-    ) . (
-      key "/members/{id}//customBoardBackgrounds/{idBackground}"
-      .  key "put"
-      .  key "parameters"
-      .  _Array
-      %~ V.cons (mkPathParam "idBackground" "The ID of the background")
-    ) . (
-      key "/members/{id}/customBoardBackgrounds/{idBackground}"
-      .  key "get"
-      .  key "parameters"
-      .  _Array
-      %~ V.cons (mkPathParam "idBackground" "The ID of the background")
-    ) . (
-      key "/enterprises/{id}/organizations/{idOrganization}"
-      .  key "delete"
-      .  key "summary"
-      .~ String "/enterprises/{id}/organizations/{idOrganization}"
-    ) . (
-      key "/enterprises/{id}/organizations/{idOrganization}"
-      .  key "delete"
-      .  key "parameters"
-      .  _Array
-      %~ (V.filter ((/=) (Just "idMember") . getStringKey "name")
-          . V.cons (mkPathParam "idOrganization" "The ID of the organization")
-      )
-    ) . (
-      key "/cards/{id}/attachments"
-      .  key "get"
-      .  key "parameters"
-      .  valuesWith "name" "fields"
-      .  keyAt "in"
-      .~ Just (String "query")
-    ) . (
-      key "/cards/{id}/attachments"
-      .  key "get"
-      .  key "parameters"
-      .  valuesWith "name" "fields"
-      .  keyAt "filter"
-      .~ Just (String "query")
-    )
+    = foldl' (.) id
+    . map applyPatch
+    $ [
+        ( key "/boards/{id}/memberships/{idMembership}"
+          .  key "put"
+          .  key "parameters"
+          .  valuesWith "name" "idMembership"
+          .  keyAt "in"
+          .~ Just (String "path")
+        ), (
+          key "/boards/{boardId}/boardStars"
+          .  key "get"
+          .  keyAt "summary"
+          .~ Just (String "/boards/{boardId}/boardStars")
+        ), (
+          key "/boards/{boardId}/boardStars"
+          .  key "get"
+          .  key "parameters"
+          .  valuesWith "name" "filter"
+          .  keyAt "in"
+          .~ Just (String "query")
+        ), (
+          key "/enterprises/{id}/members/{idMember}"
+          .  key "get"
+          .  key "parameters"
+          .  valuesWith "name" "idMember"
+          .  keyAt "in"
+          .~ Just (String "path")
+        ), (
+          key "/members/{id}/savedSearches/{idSearch}"
+          .  key "get"
+          .  key "parameters"
+          .  _Array
+          %~ V.cons (mkPathParam "idSearch" "The ID of the search")
+        ), (
+          key "/members/{id}/savedSearches/{idSearch}"
+          .  key "put"
+          .  key "parameters"
+          .  _Array
+          %~ V.cons (mkPathParam "idSearch" "The ID of the search")
+        ), (
+          key "/members/{id}//customBoardBackgrounds/{idBackground}"
+          .  key "put"
+          .  key "parameters"
+          .  _Array
+          %~ V.cons (mkPathParam "idBackground" "The ID of the background")
+        ), (
+          key "/members/{id}/customBoardBackgrounds/{idBackground}"
+          .  key "get"
+          .  key "parameters"
+          .  _Array
+          %~ V.cons (mkPathParam "idBackground" "The ID of the background")
+        ), (
+          key "/enterprises/{id}/organizations/{idOrganization}"
+          .  key "delete"
+          .  key "summary"
+          .~ String "/enterprises/{id}/organizations/{idOrganization}"
+        ), (
+          key "/enterprises/{id}/organizations/{idOrganization}"
+          .  key "delete"
+          .  key "parameters"
+          .  _Array
+          %~ (V.filter ((/=) (Just "idMember") . getStringKey "name")
+              . V.cons (mkPathParam "idOrganization" "The ID of the organization")
+          )
+        ), (
+          key "/cards/{id}/attachments"
+          .  key "get"
+          .  key "parameters"
+          .  valuesWith "name" "fields"
+          .  keyAt "in"
+          .~ Just (String "query")
+        ), (
+          key "/cards/{id}/attachments"
+          .  key "get"
+          .  key "parameters"
+          .  valuesWith "name" "filter"
+          .  keyAt "in"
+          .~ Just (String "query")
+        )
+      ]
+
+  -- Sanity check for patches that don't work as expected
+  applyPatch f x = y' where
+    y = f x
+    y' | x == y    = error "Patch was a no-op"
+       | otherwise = y
 
   mkPathParam name description
     = mkObject [
